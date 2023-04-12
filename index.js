9ad0f7ec91acd511313b275dc295c72ec57de059
Pigstand <52176535+Pigstand@users.noreply.github.com>
2023-04-11 23:05:07 -0500

    Trait count adjustment
    Added a new adjustment factor to the rarity score calculation that takes into account the number of trait values an NFT has. This adjustment ensures that NFTs with different numbers of trait values can be compared fairly, as an NFT with more trait values is likely to have a higher rarity score simply because it has more opportunities for rare trait values to occur.
    
    The adjustment factor is calculated by dividing 1 by the number of trait values an NFT has, and is stored in the traitCountFactor variable. The rarity score for each NFT is then multiplied by this adjustment factor, resulting in an adjusted rarity score that reflects the NFT's rarity relative to its number of trait values.

--- index.js
+++ index.js
@@ -3,14 +3,6 @@ const _ = require('lodash');
 
 const METADATA_LIST_PATH = 'metadata_list.json'
 
-/**
- *  CALCULATION METHOD FROM: https://raritytools.medium.com/ranking-rarity-understanding-rarity-calculation-methods-86ceaeb9b98c
- * 
- *  [Rarity Score for a Trait Value] = 1 / ([Number of Items with that Trait Value] / [Total Number of Items in Collection])
- * 
- *  The total Rarity Score for an NFT is the sum of the Rarity Score of all of it’s trait values.
- */
-
 function calculateRarity() {
     const metadataList = JSON.parse(fs.readFileSync(METADATA_LIST_PATH));
 
@@ -26,7 +18,8 @@ function calculateRarity() {
 
     for (let item of metadataList) {
         let score = 0;
-        let attributesWithRarities = []
+        let attributesWithRarities = [];
+        let traitCountFactor = 1 / item.attributes.length;
 
         for (let { trait_type, value } of item.attributes) {
             const occurence = ((traitsAndValuesWithCounts[trait_type][value] / metadataList.length) * 100).toFixed(2) + '%';
@@ -34,21 +27,22 @@ function calculateRarity() {
             attributesWithRarities.push({ trait_type, value, occurence });
         }
 
-        itemsWithRarities.push({ edition: item.edition, rarity_score: parseFloat(score.toFixed(2)), attributes: attributesWithRarities });
+        let adjustedScore = score * traitCountFactor;
+        itemsWithRarities.push({ edition: item.edition, rarity_score: parseFloat(adjustedScore.toFixed(2)), attributes: attributesWithRarities });
     }
 
     itemsWithRarities = itemsWithRarities.sort((a, b) => {
         if (a.rarity_score > b.rarity_score)      return -1;
         else if (b.rarity_score > a.rarity_score) return 1;
         else                                      return 0;
-    })
+    });
     
     itemsWithRarities.forEach((item, index) => {
         let attributes = _.cloneDeep(item.attributes);
         delete item.attributes;
         item.rank = index + 1;
         item.attributes = attributes;
-    })
+    });
 
     fs.writeFileSync(`rarity_scores.json`, JSON.stringify(itemsWithRarities, null, 4));
 }
