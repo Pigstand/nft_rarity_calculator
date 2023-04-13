e15df356cb90c6f64637dfea93ab58b6c97291f4
Pigstand <52176535+Pigstand@users.noreply.github.com>
2023-04-13 18:53:10 -0500

    Added formatting options
    In this update type is a variable initialized with a string value of either "1" or "2" It is used to determine the type of formatting used in the rarity_scores.json
    
    If type is less than or equal to 1, it will use minimal formatting, and if type is greater than 1, it will use maximum formatting. 
    
    ***** Example of minimum formatting *****
    {
        "1": {
            "rank": 4424
        },
        "2": {
            "rank": 4278
        },
        "3": {
            "rank": 220
        }
    }
    
    ***** Example of maximum formatting *****
    
     {
            "edition": 2513,
            "rarity_score": 40.69,
            "rank": 1707,
            "attributes": [
                {
                    "trait_type": "Background",
                    "value": "Peach",
                    "occurence": "21.64%"
                },
                {
                    "trait_type": "Fur",
                    "value": "I am Pastel Red",
                    "occurence": "0.90%"
                },
                {
                    "trait_type": "Clothes",
                    "value": "Pink Bones",
                    "occurence": "1.91%"
                },
                {
                    "trait_type": "Eyes",
                    "value": "Emptiness",
                    "occurence": "4.31%"
                },
                {
                    "trait_type": "Mouth",
                    "value": "Yep",
                    "occurence": "6.35%"
                },
                {
                    "trait_type": "Hat",
                    "value": "Ready to sleep",
                    "occurence": "2.72%"
                }
            ]
        },
        {
            "edition": 1298,
            "rarity_score": 40.68,
            "rank": 1708,
            "attributes": [
                {
                    "trait_type": "Background",
                    "value": "Pink Grape",
                    "occurence": "21.10%"
                },
                {
                    "trait_type": "Fur",
                    "value": "Taped Up",
                    "occurence": "1.10%"
                },
                {
                    "trait_type": "Clothes",
                    "value": "Full of Spells",
                    "occurence": "1.82%"
                },
                {
                    "trait_type": "Eyes",
                    "value": "I Don_t See This",
                    "occurence": "4.47%"
                },
                {
                    "trait_type": "Mouth",
                    "value": "I am Ready",
                    "occurence": "3.59%"
                },
                {
                    "trait_type": "Hat",
                    "value": "Sad",
                    "occurence": "2.31%"
                }
            ]
        }

--- index.js
+++ index.js
@@ -4,6 +4,58 @@ const _ = require('lodash');
 const METADATA_LIST_PATH = 'metadata_list.json'
 
 function calculateRarity() {
+    // *******  type 1 = minimal formatting  *******
+    // *******  type 2 = maximum formatting  *******
+    
+    const type = "1"; 
+
+    if (type <= 1) {
+    const metadataList = JSON.parse(fs.readFileSync(METADATA_LIST_PATH));
+
+    let traitsAndValuesWithCounts = {};
+    let itemsWithRarities = [];
+
+    for (let { attributes, edition } of metadataList) {
+        for (let { trait_type, value } of attributes) {
+            let currentValue = _.get(traitsAndValuesWithCounts, `${trait_type}.${value}`, 0);
+            _.set(traitsAndValuesWithCounts, `${trait_type}.${value}`, currentValue + 1);
+        }
+        itemsWithRarities.push({ edition });
+    }
+
+    for (let [index, item] of metadataList.entries()) {
+        let score = 0;
+        let attributesWithRarities = [];
+        let traitCountFactor = "1" / item.attributes.length;
+
+        for (let { trait_type, value } of item.attributes) {
+            const occurence = ((traitsAndValuesWithCounts[trait_type][value] / metadataList.length) * 100).toFixed(2) + '%';
+            score += 1 / (traitsAndValuesWithCounts[trait_type][value] / metadataList.length);
+            attributesWithRarities.push({ trait_type, value, occurence });
+        }
+
+        let adjustedScore = score * traitCountFactor;
+        let rank = itemsWithRarities.filter((item) => item.edition === item.edition)[0].rank;
+        itemsWithRarities[index].rarity_score = parseFloat(adjustedScore.toFixed(2));
+        itemsWithRarities[index].attributes = attributesWithRarities;
+        itemsWithRarities[index].rank = rank;
+    }
+
+    itemsWithRarities = itemsWithRarities.sort((a, b) => {
+        if (a.rarity_score > b.rarity_score)      return -1;
+        else if (b.rarity_score > a.rarity_score) return 1;
+        else                                      return 0;
+    });
+    
+    let output = {};
+    itemsWithRarities.forEach((item, index) => {
+        output[item.edition] = { rank: index + 1 };
+    });
+
+    fs.writeFileSync(`rarity_scores.json`, JSON.stringify(output, null, 4));
+
+} else {
+    
     const metadataList = JSON.parse(fs.readFileSync(METADATA_LIST_PATH));
 
     let traitsAndValuesWithCounts = {};
@@ -45,6 +97,7 @@ function calculateRarity() {
     });
 
     fs.writeFileSync(`rarity_scores.json`, JSON.stringify(itemsWithRarities, null, 4));
+}
 }
 
 calculateRarity();
